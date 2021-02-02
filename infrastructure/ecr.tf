358f1ce4f07d42e5f46f1d0df0332c4bd1c393a9
Tu Nguyen <im2nguyen@users.noreply.github.com>
2021-02-01 22:46:51 -0800

    update values

--- infrastructure/ecr.tf
+++ infrastructure/ecr.tf
@@ -1,3 +1,36 @@
 resource "aws_ecr_repository" "twv-sample-app" {
   name = "twv-sample-app-image"
 }
+
+resource "aws_ecr_repository_policy" "twv-sample-app" {
+  repository = aws_ecr_repository.twv-sample-app.name
+
+  policy = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "Waypoint",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:PutImage",
+                "ecr:InitiateLayerUpload",
+                "ecr:UploadLayerPart",
+                "ecr:CompleteLayerUpload",
+                "ecr:DescribeRepositories",
+                "ecr:GetRepositoryPolicy",
+                "ecr:ListImages",
+                "ecr:DeleteRepository",
+                "ecr:BatchDeleteImage",
+                "ecr:SetRepositoryPolicy",
+                "ecr:DeleteRepositoryPolicy"
+            ]
+        }
+    ]
+}
+EOF
+}
