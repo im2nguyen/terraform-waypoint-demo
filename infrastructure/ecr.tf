3a1e5036ed247c9deb978d4f2e96b804e8c9cba6
Tu Nguyen <im2nguyen@users.noreply.github.com>
2021-02-01 23:23:46 -0800

    update to personal aws account

--- infrastructure/ecr.tf
+++ infrastructure/ecr.tf
@@ -2,35 +2,9 @@ resource "aws_ecr_repository" "twv-sample-app" {
   name = "twv-sample-app-image"
 }
 
-resource "aws_ecr_repository_policy" "twv-sample-app" {
-  repository = aws_ecr_repository.twv-sample-app.name
+# module "ecr_ci_twv-sample-app" {
+#   source = "trussworks/iam-ecr-ci/aws"
 
-  policy = <<EOF
-{
-    "Version": "2008-10-17",
-    "Statement": [
-        {
-            "Sid": "Waypoint",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "ecr:GetDownloadUrlForLayer",
-                "ecr:BatchGetImage",
-                "ecr:BatchCheckLayerAvailability",
-                "ecr:PutImage",
-                "ecr:InitiateLayerUpload",
-                "ecr:UploadLayerPart",
-                "ecr:CompleteLayerUpload",
-                "ecr:DescribeRepositories",
-                "ecr:GetRepositoryPolicy",
-                "ecr:ListImages",
-                "ecr:DeleteRepository",
-                "ecr:BatchDeleteImage",
-                "ecr:SetRepositoryPolicy",
-                "ecr:DeleteRepositoryPolicy"
-            ]
-        }
-    ]
-}
-EOF
-}
+#   ecr_repo = aws_ecr_repository.twv-sample-app.name
+#   ci_name  = "waypoint"
+# }
